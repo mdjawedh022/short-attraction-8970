b77ee34a69c0374c338ea49dd0e80afa1fd6c130
Md Jawed <mdjawedh22@gmail.com>
2022-11-12 00:51:30 +0530

    done _landing_page

---
+++ landing_page/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AllPage from './Components/AllPage';
+
+function App() {
+  return (
+    <div className="App">
+      <AllPage/>
+    </div>
+  );
+}
+
+export default App;
